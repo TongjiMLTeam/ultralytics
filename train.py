2db70542894edb08993a8a97706c7e34e7fc6bb7
gravity97 <lizimo0908@gmail.com>
2024-06-09 14:55:01 +0800

    update SlimNeck

---
+++ train.py
@@ -0,0 +1,21 @@
+import warnings
+warnings.filterwarnings('ignore')
+from ultralytics import YOLO
+
+model = YOLO("/root/ultralytics/yolov8-SlimNeck+GSConv.yaml")
+model.train(data = "../autodl-tmp/ObstacleDataset/dataset/obstacle.yaml",
+            cache=False,
+            imgsz=640,
+            epochs=250,
+            batch=64,
+            close_mosaic=10,
+            workers=0,
+            device='0',
+            optimizer='SGD', # using SGD
+            # resume='', # last.pt path
+            # amp=False, # close amp
+            # fraction=0.2,
+            project='runs/train',
+            name='SlimNeck',
+            multi_scale=True
+            )
